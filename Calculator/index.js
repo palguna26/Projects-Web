e17f0c9094a84529a19ad54f16bad8adc8184e4b
palguna26 <142148016+palguna26@users.noreply.github.com>
2024-05-30 21:23:38 +0530

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,26 @@
+//calculator
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "symbolError";
+    }
+}
+
+function backspace(){
+    var value = document.getElementById("display").value;
+    document.getElementById("display").value = value.substr(0, value.length - 1);
+}
